ddcbc44f417416d9d757d49692acaf94c586f213
Kunjan Sanghavi <kunjan.sanghavi#gmail.com>
2019-10-07 15:42:45 -0400

    Adding console app to connect to web api.

--- ConnectWebApi/Program.cs
+++ ConnectWebApi/Program.cs
@@ -1,18 +1,65 @@
-﻿using System;
+﻿using EmployeeDataAccess;
+using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ConnectWebApi
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var yourusername = "male";
+            var yourpwd = "male";
+
 
             HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44386/api/");
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Basic", Convert.ToBase64String(
+                    System.Text.ASCIIEncoding.ASCII.GetBytes(
+                         $"{yourusername}:{yourpwd}"
+                        )));
+
+
+            var responseTask = client.GetAsync("Emp");
+            responseTask.Wait();
+
+            if (responseTask.Result.IsSuccessStatusCode)
+            {
+                var returnedEmp = responseTask.Result.Content.ReadAsAsync<Employee[]>();
+                returnedEmp.Wait();
+
+                foreach (Employee e in returnedEmp.Result)
+                {
+                    Console.WriteLine(e.FirstName);
+                }
+
+
+
+
+            }
 
 
 
         }
+
+        static void ShowEmployee(Employee e)
+        {
+            Console.WriteLine($"Name: {e.FirstName}\t" + $"{e.LastName}\tGender: {e.Gender}");
+        }
+
+
+        //static async Task<Employee> GetEmployeeAsync(string path)
+        //{
+        //    Employee emp = null;
+
+
+
+        //}
     }
 }
---
+++ ConnectWebApiDotNet/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectWebApiDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44386/api/");
+
+
+
+
+            var responseTask = client.GetAsync("Emp");
+            responseTask.Wait();
+
+            if (responseTask.Result.IsSuccessStatusCode)
+            {
+                var returnedEmp = responseTask.Result.Content.re
+            }
+
+        }
+    }
+}
